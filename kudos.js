49d7466eb2580d840b92817883bf47d961ab8920
masukomi <masukomi@masukomi.org>
2012-05-11 09:39:56 -0400

    improved everything

--- kudos.js
+++ kudos.js
@@ -1,39 +1,62 @@
+var sending_kudo = false;
 
-	function startKudoing(element) {
-		element.oldKudoText = element.children("p.count").html();
-		element.children("p.count").hide();
-		element.append('<p class="count notice"><span class="dont-move">Don\'t move...</span></p>');
-		element.addClass("filling").removeClass("animate");
-		element.parent("figure").addClass("filling");
-		sendKudo(a);
+function sendKudoInfoToServer(element_id){
+	// strip any unnecessary characters from the element_id
+	// to make it match the id of the post on your server
+	// send an ajax call to your server that the kudo for that 
+	// post has been incremented.
+	if (! sending_kudo){
+		sending_kudo = true;
+			// just to guarantee you don't accidentally send two 
+			// right after each other.
+		console.log("kudo sent");
+		setTimeout("sending_kudo = false;", 50);
 	}
-	function endKudoing(element) {
-		
-		if (element.hasClass("kudoable")){
-			element.removeClass("filling").addClass("animate");
-			element.parent("figure").removeClass("filling");
-			element.children("p.count").hide().html(element.oldKudoText);
-			element.children("p.notice").remove();
-			element.children("p.count").fadeIn("slow");
-		}
-		
+}
+
+
+function startKudoing(element) {
+	element.oldKudoText = element.children("p.count").html();
+	element.children("p.count").hide();
+	element.append('<p class="count notice"><span class="dont-move">Giving Kudos...</span></p>');
+	element.addClass("filling").removeClass("animate");
+	element.parent("figure").addClass("filling");
+	element_id = element.attr('id')
+	setTimeout("sendKudo('" + element_id +"')", 700 );
+}
+function endKudoing(element) {
+	
+	if (element.hasClass("kudoable")){
+		element.removeClass("filling").addClass("animate");
+		element.parent("figure").removeClass("filling");
+		element.children("p.count").show();
+		element.children("p.notice").remove();
+		element.children("p.count").fadeIn("slow");
 	}
-	//todo replace b with ... event / element
-	// whatever it is.
-	function sendKudo(element) {
-		element.flag = !0, 
-		element.article = element.closest("article").attr("id"),
-		$.post("/kudos.php", {
-								article: element.article,cooking: h
-							}),
+	
+}
+// whatever it is.
+function sendKudo(element_id) {
+	var element = $("#"+element_id);
+	if (element.hasClass('kudoable') && element.hasClass("filling") ){
+		element.flag = true; 
+		element.article = element.closest("article").attr("id");
+		sendKudoInfoToServer(element_id);
 		element.removeClass("animate").removeClass("kudoable").removeClass("filling").addClass("completed");
 		element.parent("figure").removeClass("filling");
 		$.cookie(element.article, true);
-		newnum = parseInt(element.oldKudoText) + 1;
-		element.newtext = newnum + " " + '<span class="identifier">Kudos</span>';
+		var count = $('#'+element_id+'-count');
+		newnum = parseInt(count.text()) + 1;
+		count.html(newnum);
 		element.children("p.notice").hide().remove();
-		element.children("p.count").html(element.newtext).fadeIn();
+		element.children("p.count").show();
+		count.fadeIn();
 	}
+}
+
+
+$(function() {
+
 
 	$("a.kudos").each(function(e) {
 		// test if they've already left a kudo for each article by 
@@ -63,7 +86,6 @@
 				var k = $(this);
 				endKudoing(k);
 			});
-	//TODO replace b with the event
 	$("a.kudos").live("touchstart", function(b) {
 			var k = $(this);
 			if (k.hasClass("kudoable")){
@@ -79,3 +101,5 @@
 			b.preventDefault();
 			return false;
 		});
+	}
+);
