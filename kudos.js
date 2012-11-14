2928a7c1014a03d2a151170eed05744b8dfac816
Matt Shwery <mshwery@gmail.com>
2012-11-14 11:23:13 -0600

    remove extra css, add animation, clean up js and markup, removable kudos

--- kudos.js
+++ kudos.js
@@ -14,92 +14,86 @@ function sendKudoInfoToServer(element_id){
 	}
 }
 
+function removeKudo(element) {
+	sendKudo(element.attr('id'));
+}
+
 
 function startKudoing(element) {
-	element.oldKudoText = element.children("p.count").html();
-	element.children("p.count").hide();
+	element.oldKudoText = element.find("a.count").html();
 	//element.append('<p class="count notice"><span class="dont-move">Giving Kudos...</span></p>');
-	element.addClass("filling").removeClass("animate");
-	element.parent("figure").addClass("filling");
-	element_id = element.attr('id')
+	element.addClass("active");
+	element_id = element.attr('id');
 	setTimeout("sendKudo('" + element_id +"')", 700 );
 }
-function endKudoing(element) {
-	
+
+function endKudoing(element) {	
 	if (element.hasClass("kudoable")){
-		element.removeClass("filling").addClass("animate");
-		element.parent("figure").removeClass("filling");
-		element.children("p.count").show();
-		//element.children("p.notice").remove();
-		element.children("p.count").fadeIn("slow");
+		element.removeClass("active");
 	}
-	
 }
+
 // whatever it is.
 function sendKudo(element_id) {
 	var element = $("#"+element_id);
-	if (element.hasClass('kudoable') && element.hasClass("filling") ){
-		element.flag = true; 
-		element.article = element.closest("article").attr("id");
-		sendKudoInfoToServer(element_id);
-		element.removeClass("animate").removeClass("kudoable").removeClass("filling").addClass("completed");
-		element.parent("figure").removeClass("filling");
-		$.cookie(element.article, true);
-		var count = $('#'+element_id+'-count');
-		newnum = parseInt(count.text()) + 1;
-		count.html(newnum);
-		//element.children("p.notice").hide().remove();
-		element.children("p.count").show();
-		count.fadeIn();
-	}
+	element.flag = true; 
+	element.article = element.closest("article").attr("id");
+	sendKudoInfoToServer(element_id);
+	var increment = element.hasClass('kudoable') ? 1 : -1;
+	element.removeClass("active").toggleClass("kudoable complete");
+	$.cookie(element.article, true);
+	var count = element.find('a.count .num');
+	newnum = parseInt(count.text()) + increment;
+	count.html(newnum);
 }
-
 
 $(function() {
 
 
-	$("a.kudos").each(function(e) {
+	$("figure.kudo").each(function(e) {
 		// test if they've already left a kudo for each article by 
 		// checking their cookies. 
 		// TODO: this WILL fail when you hit the max number of cookies for a site.
 		// remove their ability to give it a kudo if they've already done so.
 		var id = $(this).closest("article").attr("id");
 		if ($.cookie(id)){
-			$(this).removeClass("animate").removeClass("kudoable").addClass("completed");
+			$(this).removeClass("kudoable").addClass("complete");
 		} // otherwise the cookie is null and they haven't given a kudo
 	});
 	$.kudo = {};
 	$.kudo.flag = false; 
 	$.kudo.article = false;
 	
-	$("a.kudos").click(function(e) {
+	$("a.kudobject").click(function(e) {
+		var k = $(this).closest('.kudo');
+		if (k.hasClass("complete")) {
+			removeKudo(k);
+		}
+		else {
 			e.preventDefault();
-			return false;
-		});
-	
-	$("a.kudos").mouseenter(function() {
-			var k = $(this);
-			if (k.hasClass("kudoable")){
-				startKudoing(k);
-			}
-		}).mouseleave(function() {
-				var k = $(this);
-				endKudoing(k);
-			});
-	$("a.kudos").live("touchstart", function(b) {
-			var k = $(this);
-			if (k.hasClass("kudoable")){
-				startKudoing(k);
-			}
-			b.preventDefault();
-			return false;
-		});
+			return false;				
+		}
+	}).mouseenter(function() {
+		var k = $(this).closest('.kudo');
+		if (k.hasClass("kudoable")){
+			startKudoing(k);
+		}
+	}).mouseleave(function() {
+		var k = $(this).closest('.kudo');
+		endKudoing(k);
+	}).live("touchstart", function(b) {
+		var k = $(this).closest('.kudo');
+		if (k.hasClass("kudoable")){
+			startKudoing(k);
+		}
+		b.preventDefault();
+		return false;
+	}).live("touchend", function(b) {
+		var k = $(this).closest('.kudo');
+		endKudoing(k); 
+		b.preventDefault();
+		return false;
+	});
 	//TODO replace b with the event
-	$("a.kudos").live("touchend", function(b) {
-			var k = $(this);
-			endKudoing(k); 
-			b.preventDefault();
-			return false;
-		});
 	}
 );
